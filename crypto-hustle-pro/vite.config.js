bb1d3088082442e446c7e24e4b4feca9e834f80c
C0cuy0 <am.private.g@gmail.com>
2025-04-05 20:16:54 -0600

    fixing...

--- crypto-hustle-pro/vite.config.js
+++ crypto-hustle-pro/vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/crypto-hustler-pro/',
+  base: '/crypto-hustler-pro/crypto-hustle-pro/',
   plugins: [react()],
 })
